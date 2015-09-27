0a0cebae19a9f2456c886bc2d872ea8cf923496e
Jacek Rzeniewicz <jacek@rzrz.pl>
2015-09-27 14:46:03 +0100

    Finish Chapter06 exercises

--- src/Chapter06.hs
+++ src/Chapter06.hs
@@ -26,3 +26,27 @@ elem :: Eq a => a -> [a] -> Bool
 elem _ [] = False
 elem y (x:xs) = if (x == y) then True else Chapter06.elem y xs
 
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys) = if x < y then x : merge xs (y:ys) else y : merge (x:xs) ys
+
+halve :: [a] -> ([a], [a])
+halve xs = (Prelude.take n xs, drop n xs)
+			where n = (length xs) `div` 2
+
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort (x:[]) = [x]
+msort xs = merge (msort part1) (msort part2)
+			where (part1, part2) = halve xs
+
+sum :: Num a => [a] -> a
+sum = foldr (+) 0
+
+take :: Int -> [a] -> [a]
+take 0 _ = []
+take n (x:xs) = x : Chapter06.take (n-1) xs
+
+last :: [a] -> a
+last = foldr1 (\x y -> y)
